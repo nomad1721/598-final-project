e67fab11053a1dedbf85394c89839923b27fe56f
nomad1721 <jmbhcde@uw.edu>
2017-03-10 13:05:12 -0800

    tried changing color fills to variables

--- simon_game/sketch.js
+++ simon_game/sketch.js
@@ -1,18 +1,27 @@
 var displacement = 25;
+var darkRed = color(250, 0, 15);
+var darkBlue = color(0, 100, 205)
+var darkYellow = color(235, 190, 0);
+var darkGreen = color(0, 200, 95);
+var lightRed = color(255, 128, 128);
+var lightBlue = color(128, 179, 255);
+var lightYellow = color(255, 230, 128);
+var lightGreen = color (141, 211, 95);
 
 function setup() {
   createCanvas(800, 800);
   background(235);
   noStroke();
+  Squares();
 }
 
 function draw() {
-  squares();
+  
 }
 
 
 
-function squares() {
+function Squares() {
    redSquare();
    blueSquare();
    yellowSquare();
@@ -22,21 +31,22 @@ function squares() {
 
 
 function redSquare() {
-  fill(250, 0, 15);
+  fill(darkRed);
   rect(width/2 + displacement, displacement, 350, 350);
+  //if(mouseX > width/2 + displacement)
 }
 
 function blueSquare() {
-  fill(0, 100, 205);
+  fill(darkBlue);
   rect(width/2 + displacement, height/2 + displacement, 350, 350);
 }
 
 function yellowSquare() {
-  fill(235, 190, 0);
+  fill(darkYellow);
   rect(displacement, height/2 + displacement, 350, 350);
 }
 
 function greenSquare() {
-  fill(0, 200, 95);
+  fill(darkGreen);
   rect(displacement, displacement, 350, 350);
 }
