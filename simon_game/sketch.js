09f6e81677ef2c78cf391591a36735f4b4ff5228
nomad1721 <jmbhcde@uw.edu>
2017-03-12 16:10:43 -0700

    added non functional blinking

--- simon_game/sketch.js
+++ simon_game/sketch.js
@@ -5,7 +5,8 @@
 */
 var page = 0; //call back to original page 
 var displacement = 25;
-var blinkSequence = [0, 1, 2, 3];
+var blinkSequence = [0, 1, 2, 3, 0, 1, 2, 3];
+var delay = 1000; // 1 second (in milliseconds)
 
 function setup() {
   createCanvas(800, 800);
@@ -14,18 +15,19 @@ function setup() {
   textSize(40);
 }  
 
-
+// 
 function draw() {
  background(235); 
  if (page == 0) {  // displays start game page
       text('Click to Start', 250, height/2);
   } else { // if page !0 then switches to game screen
       squares();
-    
+      //startBlinks();
+      
   }
   
 }
-// changes between screens 
+// changes between screens ----------------------------------------------
 function mousePressed() {
   page = 1;
 }
@@ -40,7 +42,7 @@ function squares() {
   
 }
 
-
+// defines individual colored sqares -------------------------------------
 function redSquare() {
   if (mouseX >= 425 && mouseX <= 775 && mouseY <= 375 && mouseY >= 25 && mouseIsPressed) {
     fill(255,128, 128);
@@ -78,19 +80,20 @@ function greenSquare() {
 }
 
 // read values in flashSequence array -----------------------------------
-for (var i = 0; i < 8; i++) {
-  blinkSequence[i] = i
-  if(i == 0) {
-    redBlink();
-  } else if (i == 1) {
-    blueBlink();
-  } else if (i == 2) {
-    yellowBlink();
-  } else if (i == 3) {
-    greenBlink();
+function startBlinks() {
+  for (var i = 0; i < blinkSequence.length; i++) {
+  //blinkSequence[i] = i
+    if(blinkSequence[i] === 0) {
+      redBlink();
+    } else if (blinkSequence[i] === 1) {
+      blueBlink();
+    } else if (blinkSequence[i] === 2) {
+      yellowBlink();
+    } else if (blinkSequence[i] === 3) {
+      greenBlink();
+    }
   }
 }
-
 
 // draws color "flash"---------------------------------------------------
 function redBlink() {
