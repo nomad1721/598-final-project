195b55fa689f15bfbe6ae9edaeb7d739cedc3e79
nomad1721 <jmbhcde@uw.edu>
2017-03-12 20:02:28 -0700

    added pieces of Susan's code (THANK YOU) along with some things from Sam

--- simon_game/sketch.js
+++ simon_game/sketch.js
@@ -5,47 +5,89 @@
 */
 var page = 0; //call back to original page 
 var displacement = 25;
-var blinkSequence = [0, 1, 2, 3, 0, 1, 2, 3];
-var delay = 1000; // 1 second (in milliseconds)
+var time = 0;
+var blinkSequence = [blinkRedSquare,blinkBlueSquare, blinkGreenSquare, blinkYellowSquare];
+var timeValues = [15, 45, 75, 105];
 
 function setup() {
   createCanvas(800, 800);
   background(235);
   noStroke();
   textSize(40);
+  shuffle(blinkSequence, true);
 }  
 
-// 
 function draw() {
  background(235); 
  if (page == 0) {  // displays start game page
       text('Click to Start', 250, height/2);
   } else { // if page !0 then switches to game screen
-      squares();
-      //startBlinks();
-      
+      startGame();
   }
   
 }
-// changes between screens ----------------------------------------------
+// changes between screens 
 function mousePressed() {
   page = 1;
 }
 
-
-// draws colored squares of Simon game ----------------------------------
 function squares() {
    redSquare();
    blueSquare();
    yellowSquare();
    greenSquare();
-  
 }
 
-// defines individual colored sqares -------------------------------------
+function startGame() {
+for (i = 0; i < blinkSequence.length; i++) {
+    blinkSequence[i](timeValues[i], timeValues[i] + 15);
+  }
+  time = time + 1;
+}
+
+//BLINKING SQUARES
+function blinkRedSquare(timeStart, timeEnd) {
+  if (time < timeStart || time > timeEnd) {
+    fill(250, 0, 15);    
+  } else if (time < timeEnd) {
+    fill(255,128,128);
+  }
+  rect(width/2 + displacement, displacement, 350, 350);
+}
+
+function blinkBlueSquare(timeStart, timeEnd) {
+  if (time < timeStart || time > timeEnd) {
+    fill(0, 100, 205);  
+  } else if (time < timeEnd) {
+    fill(128, 179, 255);
+  }
+  rect(width/2 + displacement, height/2 + displacement, 350, 350);
+}
+
+function blinkYellowSquare(timeStart, timeEnd) {
+  if (time < timeStart || time > timeEnd) {
+    fill(255, 230, 128);   
+  } else if (time < timeEnd) {
+    fill(235, 190, 0);
+  }
+  rect(displacement, height/2 + displacement, 350, 350);
+}
+
+function blinkGreenSquare(timeStart, timeEnd) {
+  if (time < timeStart || time > timeEnd) {
+    fill(0, 200, 95);
+  } else if (time < timeEnd) {
+    fill(124,252,0);
+  }
+  rect(displacement, displacement, 350, 350);
+}
+
+
+//Player input mode
 function redSquare() {
+  noStroke();
   if (mouseX >= 425 && mouseX <= 775 && mouseY <= 375 && mouseY >= 25 && mouseIsPressed) {
-    fill(255,128, 128);
+    fill(255,128,128);
   } else {
     fill(250, 0, 15);
   }
@@ -53,6 +95,7 @@ function redSquare() {
 }
 
 function blueSquare() {
+  noStroke();
   if(mouseX >= 425 && mouseX <= 775 && mouseY >= 425 && mouseY <= 775 && mouseIsPressed) {
     fill(128, 179, 255);
   } else {
@@ -62,6 +105,7 @@ function blueSquare() {
 }
 
 function yellowSquare() {
+  noStroke();
   if(mouseX <= 375 && mouseX >= 25 && mouseY >= 425 && mouseY <= 775 && mouseIsPressed) {
     fill(255, 230, 128);
   } else {
@@ -71,47 +115,11 @@ function yellowSquare() {
 }
 
 function greenSquare() {
+  noStroke();
   if(mouseX <= 375 && mouseX >= 25 && mouseY <= 375 && mouseY >= 25 && mouseIsPressed) {
-    fill(141, 211, 95);
+    fill(124,252,0);
   } else {
-    fill(0, 200, 95);
+    fill(0,200,95);
   }
-  rect(displacement, displacement, 350, 350);
-}
-
-// read values in flashSequence array -----------------------------------
-function startBlinks() {
-  for (var i = 0; i < blinkSequence.length; i++) {
-  //blinkSequence[i] = i
-    if(blinkSequence[i] === 0) {
-      redBlink();
-    } else if (blinkSequence[i] === 1) {
-      blueBlink();
-    } else if (blinkSequence[i] === 2) {
-      yellowBlink();
-    } else if (blinkSequence[i] === 3) {
-      greenBlink();
-    }
-  }
-}
-
-// draws color "flash"---------------------------------------------------
-function redBlink() {
-  fill(255, 128, 128);
-  rect(width/2 + displacement, displacement, 350, 350);
-}
-
-function blueBlink() {
-  fill(128, 179, 255);
-  rect(width/2 + displacement, height/2 + displacement, 350, 350);
-}
-
-function yellowBlink() {
-  fill(255, 230, 128);
-  rect(displacement, height/2 + displacement, 350, 350);
-}
-
-function greenBlink() {
-  fill(141, 211, 95);
   rect(displacement, displacement, 350, 350);
 }
